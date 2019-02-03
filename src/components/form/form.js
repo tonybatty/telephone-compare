ef8cc81525037fb11b6f2f9fbe0d792010a13717
Tony Batty <tonyeb87@gmail.com>
2019-02-03 16:58:25 +0000

    testing form with netlify

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -69,66 +69,117 @@ class Form extends Component {
 
     return (
       <form
-        name="quote"
+        name="contact"
         method="post"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
       >
-        <input type="hidden" name="form-name" value="quote" />
-        {step === 0 ? (
-          <FormStart
-            handleInputChange={this.handleInputChange}
-            nextStep={this.nextStep}
+        <input type="hidden" name="bot-field" />
+        <div className="field half first">
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" id="name" />
+        </div>
+        <div className="field half">
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" id="email" />
+        </div>
+        <div className="field">
+          <label htmlFor="message">Message</label>
+          <textarea name="message" id="message" rows="6" />
+        </div>
+
+        <div className="form-check">
+          <input
+            type="radio"
+            name="handsets"
+            value="0-10"
+            className="form-check__input"
+            checked="true"
           />
-        ) : null}
-        {step === 1 ? (
-          <FormStep1
-            handleInputChange={this.handleInputChange}
-            handsets={handsets}
-            nextStep={this.nextStep}
+          <label htmlFor="handsets">0-10</label>
+        </div>
+
+        <div className="form-check">
+          <input
+            type="radio"
+            name="handsets"
+            value="10-50"
+            className="form-check__input"
           />
-        ) : null}
-        {step === 2 ? (
-          <FormStep2
-            handleInputChange={this.handleInputChange}
-            sites={sites}
-            nextStep={this.nextStep}
-            prevStep={this.prevStep}
-          />
-        ) : null}
-        {step === 3 ? (
-          <FormStep3
-            handleInputChange={this.handleInputChange}
-            broadband={broadband}
-            nextStep={this.nextStep}
-            prevStep={this.prevStep}
-          />
-        ) : null}
-        {step === 4 ? (
-          <FormStep4
-            handleInputChange={this.handleInputChange}
-            system={system}
-            nextStep={this.nextStep}
-            prevStep={this.prevStep}
-          />
-        ) : null}
-        {step === 5 ? (
-          <FormStep5
-            handleInputChange={this.handleInputChange}
-            timespan={timespan}
-            nextStep={this.nextStep}
-            prevStep={this.prevStep}
-          />
-        ) : null}
-        {step === 6 ? (
-          <FormStep6
-            handleInputChange={this.handleInputChange}
-            location={location}
-            distance={distance}
-            prevStep={this.prevStep}
-          />
-        ) : null}
+          <label htmlFor="handsets">10-50</label>
+        </div>
+
+        <ul className="actions">
+          <li>
+            <input type="submit" value="Send Message" className="special" />
+          </li>
+          <li>
+            <input type="reset" value="Clear" />
+          </li>
+        </ul>
       </form>
+
+      //   <form
+      //     name="quote"
+      //     method="post"
+      //     data-netlify="true"
+      //     data-netlify-honeypot="bot-field"
+      //   >
+      //     <input type="hidden" name="form-name" value="quote" />
+      //     {step === 0 ? (
+      //       <FormStart
+      //         handleInputChange={this.handleInputChange}
+      //         nextStep={this.nextStep}
+      //       />
+      //     ) : null}
+      //     {step === 1 ? (
+      //       <FormStep1
+      //         handleInputChange={this.handleInputChange}
+      //         handsets={handsets}
+      //         nextStep={this.nextStep}
+      //       />
+      //     ) : null}
+      //     {step === 2 ? (
+      //       <FormStep2
+      //         handleInputChange={this.handleInputChange}
+      //         sites={sites}
+      //         nextStep={this.nextStep}
+      //         prevStep={this.prevStep}
+      //       />
+      //     ) : null}
+      //     {step === 3 ? (
+      //       <FormStep3
+      //         handleInputChange={this.handleInputChange}
+      //         broadband={broadband}
+      //         nextStep={this.nextStep}
+      //         prevStep={this.prevStep}
+      //       />
+      //     ) : null}
+      //     {step === 4 ? (
+      //       <FormStep4
+      //         handleInputChange={this.handleInputChange}
+      //         system={system}
+      //         nextStep={this.nextStep}
+      //         prevStep={this.prevStep}
+      //       />
+      //     ) : null}
+      //     {step === 5 ? (
+      //       <FormStep5
+      //         handleInputChange={this.handleInputChange}
+      //         timespan={timespan}
+      //         nextStep={this.nextStep}
+      //         prevStep={this.prevStep}
+      //       />
+      //     ) : null}
+      //     {step === 6 ? (
+      //       <FormStep6
+      //         handleInputChange={this.handleInputChange}
+      //         location={location}
+      //         distance={distance}
+      //         prevStep={this.prevStep}
+      //       />
+      //     ) : null}
+      //   </form>
     );
   }
 }
