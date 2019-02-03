53ac8cddbe9fea91694d66ee330c582d6e9ff915
Tony Batty <tonyeb87@gmail.com>
2019-02-03 15:37:17 +0000

    adjuseted layout for content and footer and centered

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,7 @@ import { StaticQuery, graphql } from "gatsby";
 
 import Header from "./header";
 import "../styles/main.scss";
+import "../styles/components/layout.scss";
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -19,15 +20,8 @@ const Layout = ({ children }) => (
     render={data => (
       <div className="container">
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0
-          }}
-        >
-          <main>{children}</main>
+        <div className="content">
+          <main className="content__centered">{children}</main>
         </div>
         <footer className="footer">
           Copyright © Telephone Compare {new Date().getFullYear()}. All Rights
