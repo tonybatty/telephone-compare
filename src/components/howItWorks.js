19e3081f1ce4a423341f33b8af99e9f58f5e4f64
Tony Batty <tonyeb87@gmail.com>
2019-02-04 01:43:19 +0000

    added styling to howItWorks component

--- src/components/howItWorks.js
+++ src/components/howItWorks.js
@@ -4,13 +4,13 @@ import form from "../images/form.png";
 import report from "../images/report.png";
 import profile from "../images/profile.png";
 
-const HowItWorks = ({}) => (
-  <div className="howItWorks">
+const HowItWorks = () => (
+  <div className="howItWorks container">
     <h1 className="howItWorks__title">How it works</h1>
 
     <div className="row">
       <div className="row__column">
-        <img src={form} />
+        <img src={form} alt="form" />
         <h2>Fill out the form</h2>
         <p>
           Answer a few simple questions about your business telecoms
@@ -18,12 +18,12 @@ const HowItWorks = ({}) => (
         </p>
       </div>
       <div className="row__column">
-        <img src={report} />
+        <img src={report} alt="report" />
         <h2>Receive up to five Quotes from local suppliers</h2>
         <p>They will not receive your information, you contact them</p>
       </div>
       <div className="row__column">
-        <img src={profile} />
+        <img src={profile} alt="profile" />
         <h2>View your suppliers profiles</h2>
         <p>Find out about them and see other users reviews</p>
       </div>
